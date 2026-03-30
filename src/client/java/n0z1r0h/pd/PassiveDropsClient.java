8c582f0b38c90a922a0092fb68c69f41c2fe1640
N0z1r0h <ntge31@gmail.com>
2026-03-30 14:13:24 -0400

    mod 8 files

--- src/client/java/n0z1r0h/pd/PassiveDropsClient.java
+++ src/client/java/n0z1r0h/pd/PassiveDropsClient.java
@@ -1,8 +1,8 @@
-package com.example;
+package n0z1r0h.pd;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class ExampleModClient implements ClientModInitializer {
+public class PassiveDropsClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
