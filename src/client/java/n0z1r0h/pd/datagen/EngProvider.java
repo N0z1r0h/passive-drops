700bcd6be86331a46a6536afc2d905be1fb54f27
N0z1r0h <ntge31@gmail.com>
2026-04-04 16:44:33 -0400

    Create EngProvider.java

---
+++ src/client/java/n0z1r0h/pd/datagen/EngProvider.java
@@ -0,0 +1,18 @@
+package n0z1r0h.pd.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider;
+import net.minecraft.core.HolderLookup;
+
+import java.util.concurrent.CompletableFuture;
+
+public class EngProvider extends FabricLanguageProvider {
+    protected EngProvider(FabricPackOutput dataOutput, CompletableFuture<HolderLookup.Provider> registryLookup) {
+        super(dataOutput, "en_us", registryLookup);
+    }
+
+    @Override
+    public void generateTranslations(HolderLookup.Provider holderLookup, TranslationBuilder translationBuilder) {
+        
+    }
+}
