9648ec733fc7b7b16457f61d4b7d16aada877e94
N0z1r0h <ntge31@gmail.com>
2026-04-04 16:35:08 -0400

    Create PassiveDropsDataGenerator.java

---
+++ src/client/java/n0z1r0h/pd/datagen/PassiveDropsDataGenerator.java
@@ -0,0 +1,10 @@
+package n0z1r0h.pd.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.minecraft.data.DataGenerator;
+
+public class PassiveDropsDataGenerator implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitalizeDataGenerator(FabricDataGenerator fabricDataGenerator) {}
+}
